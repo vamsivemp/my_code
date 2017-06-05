76e9932f971c031290cebb89e2890fa8944f130d
DESKTOP-LEFEHIG\irock <suryavamsi.vn366@gmail.com>
2017-06-04 19:43:10 -0600

    overlapping comment

--- Comcast/src/main/java/com/web/ComcastApplication.java
+++ Comcast/src/main/java/com/web/ComcastApplication.java
@@ -8,6 +8,6 @@ public class ComcastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComcastApplication.class, args);
-		//added new comment in trial branch
+		//added a new overlapping comment on branch trial
 	}
 }
